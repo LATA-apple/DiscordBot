b94135e886b55befb299a4d78b0e388bbba19305
Glitch (pumped-chopper) <none>
2022-05-28 04:27:35 +0000

    👽🌷 Checkpoint ./package.json:2284950/45 ./index.js:2284950/902 ./bot.js:2284950/293

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on('ready', () => {
   console.log(`${client.user.tag}にログインしました！`)
+  console.log(client)
 })
 
 client.on('message', message => {
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Response for Uptime Robot
 const http = require('http')
+
 http
-  .createServer(function(request, response) {
+  .createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.end('Discord bot is active now \n')
   })

de1c990df2a35244fd5f28aac087da4b0a244451
Glitch (bow-miniature-color) <none>
2024-02-14 14:38:03 +0000

    🐟🛀 Checkpoint ./index.js:64942737/1967

--- index.js
+++ index.js
@@ -82,56 +82,23 @@ client.on('messageCreate', async message => {
       }
     
     
-    message.channel.send(
-      {embed: {
-        author: {
-          name: "author name",
-          url: "https://discordapp.com", // nameプロパティのテキストに紐付けられるURL
-          icon_url: "https://cdn.discordapp.com/embed/avatars/0.png"
-        },
-        title: '${message.content}',
-        url: "https://discordapp.com", // titleプロパティのテキストに紐付けられるURL
-        description: "This is description. [URLを埋め込むことも出来る](https://discordapp.com)\n" +
-                     "***embedの中でもMarkDownを利用できます***",
-        color: 7506394,
-        timestamp: new Date(),
-        footer: {
-          icon_url: client.user.avatarURL,
-          text: "©️ example | footer text"
-        },
+    // Embedメッセージの作成
+    const embedMessage = {
+      embed: {
+        title: message.content, // message.contentをタイトルとして使用
         thumbnail: {
-          url: "https://cdn.discordapp.com/embed/avatars/0.png"
-        },
-        image: {
-        url: "https://cdn.discordapp.com/embed/avatars/0.png"
+          url: imageURL // imageURLをthumbnailのURLとして使用
         },
         fields: [
           {
-            name: "field :one:",
-            value: "*ここはfield 1の内容だよ*"
-          },
-          {
-            name: "field :two:",
-            value: "~~ここはfield 2の内容だよ~~"
-          },
-          {
-            name: "field :three:",
-            value: "__ここはfield 3の内容だよ__"
-          },
-          {
-            name: "inline field :cat:",
-            value: "`これはinlineのfieldだよ`",
-            inline: true
-          },
-          {
-            name: "inline field :dog:",
-            value: "[これもinlineのfieldだよ](https://discordapp.com)",
-            inline: true
+            name: key, // fieldsのname
+            value: value // fieldsのvalue
           }
+          // 他のfieldsの内容もここに追加できます
         ]
-      }}
-);
-
+      }
+    };
+    
     
     
     

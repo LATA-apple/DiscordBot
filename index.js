604c4455fb9da53767a49d0384e6593f9b20a8e8
Glitch (bow-miniature-color) <none>
2024-02-14 08:48:05 +0000

    💶👆 Checkpoint ./index.js:64942737/2825

--- index.js
+++ index.js
@@ -15,38 +15,50 @@ client.on("ready", () => {
 const databaseId = '9403ad41aa344441951044a6656d0d9a';
 const url = `https://api.notion.com/v1/databases/${databaseId}/query`;
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Notion-Version': '2022-06-28',
-  'Authorization': 'Bearer secret_yRXLwrnuBgXoquzA3L6j7dKMMIfbMSiacqMXdyFQjGV'
-};
 
-const filterData = {
-  "filter": {
-    "property": "キャラ名",
-    "title": {
-      "equals": "魈"
+client.on('messageCreate', async message => {
+  // Ignore messages from other bots
+  if (message.author.bot) return;
+  
+  // Check if the message is from the specified channel ID
+  if (message.channel.id !== '1206824509538308116') return;
+  
+  const headers = {
+    'Content-Type': 'application/json',
+    'Notion-Version': '2022-06-28',
+    'Authorization': 'Bearer secret_yRXLwrnuBgXoquzA3L6j7dKMMIfbMSiacqMXdyFQjGV'
+  };
+  
+  const filterData = {
+    "filter": {
+      "property": "キャラ名",
+      "title": {
+        "equals": message.content // メッセージの内容をキャラ名として使用
+      }
     }
-  }
-};
-
-const requestOptions = {
-  method: 'POST',
-  headers: headers,
-  body: JSON.stringify(filterData)
-};
-
-fetch(url, requestOptions)
-  .then(response => response.json())
-  .then(data => {
-    const properties = data.results[0].properties; // レスポンスのresultsからpropertiesオブジェクトを取得
-    Object.keys(properties).forEach(key => {
-      const property = properties[key]; // 各プロパティを取得
-      console.log(`Key: ${key}`);
-      console.log(property); // プロパティの内容を出力
-    });
-  })
-  .catch(error => console.error('Error:', error));
+  };
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(filterData)
+  };
+  
+  fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      const properties = data.results[0].properties; // レスポンスのresultsからpropertiesオブジェクトを取得
+      Object.keys(properties).forEach(key => {
+        const property = properties[key]; // 各プロパティを取得
+        console.log(`Key: ${key}`);
+        console.log(property); // プロパティの内容を出力
+        message.channel.send(`Key: ${key}`);
+        message.channel.send(property);
+      });
+    })
+    .catch(error => console.error('Error:', error));
+  
+});
 
 
 client.login(process.env.DISCORD_BOT_TOKEN);

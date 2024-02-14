d8a0a73a4dfd7d12b2e8532171b0ef8e008e817f
Glitch (bow-miniature-color) <none>
2024-02-14 09:23:57 +0000

    👠😼 Checkpoint ./index.js:64942737/2307

--- index.js
+++ index.js
@@ -72,11 +72,22 @@ client.on('messageCreate', async message => {
           message.channel.send(`**${key}:** ${typeof value === 'object' ? JSON.stringify(value) : value}`);
         }
       });
+    // ValueからURLを取得
+const imageURL = properties.files[0].name;
+
+// URLが存在する場合はメッセージとして送信
+if (imageURL) {
+  message.channel.send({
+    files: [imageURL]
+  });
+}
       
       // 画像のURLを取得して送信
       const imageURLs = properties['画像']?.files.map(file => file.file?.url).filter(url => url);
       if (imageURLs && imageURLs.length > 0) {
-        message.channel.send(imageURLs.join('\n'));
+        imageURLs.forEach(url => {
+          message.channel.send(url);
+        });
       }
     })
     .catch(error => console.error('Error:', error));

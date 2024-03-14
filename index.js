b7fc06359f55687dd489920bd064f16a37fd56a0
Glitch (bow-miniature-color) <none>
2024-03-14 18:11:34 +0000

    🎟💖 Checkpoint ./index.js:64942737/3287

--- index.js
+++ index.js
@@ -72,33 +72,61 @@ client.on('messageCreate', async message => {
     .setURL(notionurl)
     let sendtext = '';
     sendtext = data.results[0]?.properties["レア度"]?.select?.name;
-    embed1.addField('- '+'レア度'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'レア度'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["元素"]?.select?.name;
-    embed1.addField('- '+'元素'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'元素'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["武器種"]?.select?.name;
-    embed1.addField('- '+'武器種'+' -', sendtext);
-    sendtext = data.results[0]?.properties["育成優先度"]?.select?.name;
-    embed1.addField('- '+'育成優先度'+' -', sendtext);
-    sendtext = data.results[0]?.properties["最優先ステータス"]?.select?.name;
-    embed1.addField('- '+'最優先ステータス'+' -', sendtext);
-    sendtext = data.results[0]?.properties["推奨ステータス"]?.multi_select?.map(item => item.name).join('\n');
-    embed1.addField('- '+'推奨ステータス'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'武器種'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["特産品"]?.select?.name;
-    embed1.addField('- '+'特産品'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'特産品'+' -', sendtext),true;
+    }
     sendtext = data.results[0]?.properties["強敵"]?.select?.name;
-    embed1.addField('- '+'強敵'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'強敵'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["天賦本"]?.select?.name;
-    embed1.addField('- '+'天賦本'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'天賦本'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["天賦素材(週ボス)"]?.select?.name;
-    embed1.addField('- '+'天賦素材(週ボス)'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'天賦素材(週ボス)'+' -', sendtext,true);
+    }
+    sendtext = data.results[0]?.properties["育成優先度"]?.select?.name;
+    if (sendtext) {
+      embed1.addField('- '+'育成優先度'+' -', sendtext,true);
+    }
+    sendtext = data.results[0]?.properties["最優先ステータス"]?.select?.name;
+    if (sendtext) {
+      embed1.addField('- '+'最優先ステータス'+' -', sendtext,true);
+    }
+    sendtext = data.results[0]?.properties["推奨ステータス"]?.multi_select?.map(item => item.name).join('\n');
+    if (sendtext) {
+      embed1.addField('- '+'推奨ステータス'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["参照プロパティ"]?.rich_text?.map(item => item.plain_text).join('\n');
-    embed1.addField('- '+'参照プロパティ'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'参照プロパティ'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["推奨凸"]?.multi_select?.map(item => item.name).join('\n');
-    embed1.addField('- '+'推奨凸'+' -', sendtext);
-    sendtext = data.results[0]?.properties["おすすめ凸とその解説"]?.rich_text?.map(item => item.plain_text).join('\n');
-    embed1.addField('- '+'おすすめ凸とその解説'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'推奨凸'+' -', sendtext,true);
+    }
     sendtext = data.results[0]?.properties["おすすめ武器"]?.rich_text?.map(item => item.plain_text).join('\n');
-    embed1.addField('- '+'おすすめ武器'+' -', sendtext);
+    if (sendtext) {
+      embed1.addField('- '+'おすすめ武器'+' -', sendtext,true);
+    }
+    sendtext = data.results[0]?.properties["おすすめ凸とその解説"]?.rich_text?.map(item => item.plain_text).join('\n');
+    if (sendtext) {
+      embed1.addField('- '+'おすすめ凸とその解説'+' -', sendtext);
+    }
     const image = data.results[0].icon.external.url;
     embed1.setThumbnail(image)
     message.channel.send({ embeds: [embed1] })
